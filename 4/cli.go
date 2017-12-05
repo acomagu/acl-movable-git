ddc0a0ec18a6cefa80e0873b5133cf5c378a1620
Yuki Ito <acomagu@gmail.com>
2017-12-05 19:36:41 +0900

    [Fix] output errors to errStream

--- 4/cli.go
+++ 4/cli.go
@@ -21,7 +21,7 @@ func (c *CLI) Run(args []string) int {
 	flags.BoolVar(&unified, "u", false, "output as unified context")
 
 	if err := flags.Parse(args[1:]); err != nil {
-		fmt.Sprintln(c.errStream, err)
+		fmt.Fprintln(c.errStream, err)
 		return 1
 	}
 	for 0 < flags.NArg() {
@@ -29,32 +29,32 @@ func (c *CLI) Run(args []string) int {
 
 		err := flags.Parse(flags.Args()[1:])
 		if err != nil {
-			fmt.Sprintln(c.errStream, err)
+			fmt.Fprintln(c.errStream, err)
 			return 1
 		}
 	}
 
 	af, err := os.Open(filenames[0])
 	if err != nil {
-		fmt.Sprintln(c.errStream, err)
+		fmt.Fprintln(c.errStream, err)
 		return 1
 	}
 	defer af.Close()
 	a, err := ioutil.ReadAll(af)
 	if err != nil {
-		fmt.Sprintln(c.errStream, err)
+		fmt.Fprintln(c.errStream, err)
 		return 1
 	}
 
 	bf, err := os.Open(filenames[1])
 	if err != nil {
-		fmt.Sprintln(c.errStream, err)
+		fmt.Fprintln(c.errStream, err)
 		return 1
 	}
 	defer bf.Close()
 	b, err := ioutil.ReadAll(bf)
 	if err != nil {
-		fmt.Sprintln(c.errStream, err)
+		fmt.Fprintln(c.errStream, err)
 		return 1
 	}
 
