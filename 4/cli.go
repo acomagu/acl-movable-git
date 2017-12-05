4f730687863e00d93c532fd544aa59691db9309b
Yuki Ito <acomagu@gmail.com>
2017-12-05 19:31:14 +0900

    [Fix] parse correctly

--- 4/cli.go
+++ 4/cli.go
@@ -13,29 +13,7 @@ type CLI struct {
 }
 
 func (c *CLI) Run(args []string) int {
-	af, err := os.Open(args[1])
-	if err != nil {
-		fmt.Sprintln(c.errStream, err)
-		return 1
-	}
-	defer af.Close()
-	a, err := ioutil.ReadAll(af)
-	if err != nil {
-		fmt.Sprintln(c.errStream, err)
-		return 1
-	}
-
-	bf, err := os.Open(args[2])
-	if err != nil {
-		fmt.Sprintln(c.errStream, err)
-		return 1
-	}
-	defer bf.Close()
-	b, err := ioutil.ReadAll(bf)
-	if err != nil {
-		fmt.Sprintln(c.errStream, err)
-		return 1
-	}
+	filenames := []string{}
 
 	var unified bool
 	flags := flag.NewFlagSet("mdiff", flag.ContinueOnError)
@@ -47,11 +25,37 @@ func (c *CLI) Run(args []string) int {
 		return 1
 	}
 	for 0 < flags.NArg() {
+		filenames = append(filenames, flags.Args()[0])
+
 		err := flags.Parse(flags.Args()[1:])
 		if err != nil {
 			fmt.Sprintln(c.errStream, err)
 			return 1
 		}
+	}
+
+	af, err := os.Open(filenames[0])
+	if err != nil {
+		fmt.Sprintln(c.errStream, err)
+		return 1
+	}
+	defer af.Close()
+	a, err := ioutil.ReadAll(af)
+	if err != nil {
+		fmt.Sprintln(c.errStream, err)
+		return 1
+	}
+
+	bf, err := os.Open(filenames[1])
+	if err != nil {
+		fmt.Sprintln(c.errStream, err)
+		return 1
+	}
+	defer bf.Close()
+	b, err := ioutil.ReadAll(bf)
+	if err != nil {
+		fmt.Sprintln(c.errStream, err)
+		return 1
 	}
 
 	fmt.Fprint(c.outStream, diff(string(a), string(b), unified))
